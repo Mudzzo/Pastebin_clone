b4f6e5afce783fc88c68dc25d23f5f6bd37849ac
Mudzzo <mohamed2004kotb@gmail.com>
2025-03-22 17:32:13 +0200

    fixed a bug with search page pagination

--- search.php
+++ search.php
@@ -63,7 +63,27 @@ if(isset($_GET['query']) && !empty($_GET['query'])){
                             <?php endforeach ?>
                         </tbody>
                     </table>
-                    <?php require_once "includes/pagination_bar.php";?>
+                    <nav>
+                        <ul class="pagination justify-content-center">
+                            <?php if ($current_page > 1): ?>
+                                <li class="page-item">
+                                    <a class="page-link" href="?query=<?= urlencode($search) ?>&page=<?= $current_page - 1 ?>">Previous</a>
+                                </li>
+                            <?php endif; ?>
+
+                            <?php for ($i = 1; $i <= $total_pages; $i++): ?>
+                                <li class="page-item <?= ($i === $current_page) ? 'active' : '' ?>">
+                                    <a class="page-link" href="?query=<?= urlencode($search) ?>&page=<?= $i ?>"><?= $i ?></a>
+                                </li>
+                            <?php endfor; ?>
+
+                            <?php if ($current_page < $total_pages): ?>
+                                <li class="page-item">
+                                    <a class="page-link" href="?query=<?= urlencode($search) ?>&page=<?= $current_page + 1 ?>">Next</a>
+                                </li>
+                            <?php endif; ?>
+                        </ul>
+                    </nav>
                     
                 </div>
                 <?php require_once "includes/search_bar.php";?>
